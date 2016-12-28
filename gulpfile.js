d94a85e18c704ec3af91c9c3b7c873e0aa6ea6b1
Yohan Robert <yohan.robert@outlook.fr>
2016-12-28 14:51:13 +0100

    Track project reading completion

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
-var gulp        = require('gulp');
-var minify      = require('gulp-minify');
-var $           = require('gulp-load-plugins')();
-var concat      = require('gulp-concat');
-var connect     = require('gulp-connect');
-var clean       = require('gulp-clean');
-var runSequence = require('run-sequence');
-var bowerFiles  = require('main-bower-files');
-var ghPages     = require('gulp-gh-pages');
+var gulp        = require("gulp");
+var minify      = require("gulp-minify");
+var $           = require("gulp-load-plugins")();
+var concat      = require("gulp-concat");
+var connect     = require("gulp-connect");
+var clean       = require("gulp-clean");
+var runSequence = require("run-sequence");
+var bowerFiles  = require("main-bower-files");
+var ghPages     = require("gulp-gh-pages");
 
+var deploy = false;
 var sassPaths = [
-  'bower_components/normalize.scss/sass',
-  'bower_components/foundation-sites/scss',
-  'bower_components/motion-ui/src'
+  "bower_components/normalize.scss/sass",
+  "bower_components/foundation-sites/scss",
+  "bower_components/motion-ui/src"
 ];
 
 var seq = function(args, cb) {
@@ -34,95 +35,100 @@ var watchSeq = function() {
 /**
  * Precompile scss/sass files into dist/css/app.css.
  */
-gulp.task('sass', function() {
-  return gulp.src(['scss/app.scss'])
+gulp.task("sass", function() {
+  return gulp.src(["scss/app.scss"])
     .pipe($.sass({
       includePaths: sassPaths,
-      outputStyle: 'compressed'
+      outputStyle: "compressed"
     })
-    .on('error', $.sass.logError))
+    .on("error", $.sass.logError))
     .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
+      browsers: ["last 2 versions", "ie >= 9"]
     }))
-    .pipe(gulp.dest('dist/css/'));
+    .pipe(gulp.dest("dist/css/"));
 });
 
 /**
  * Minify js files and copy them to dist/js folder.
  */
-gulp.task('js', function() {
-  return gulp.src(bowerFiles({ filter: /^.*.js$/ }).concat('js/*.js'))
-    .pipe(minify({ noSource: true }))
-    .pipe(concat('app.js'))
-    .pipe(gulp.dest('dist/js'));
+gulp.task("js", function() {
+  var task = gulp.src(bowerFiles({ filter: /^.*.js$/ }).concat("js/*.js"));
+  if(deploy){
+    task = task.pipe(minify({ noSource: true }))
+  }
+  return task.pipe(concat("app.js"))
+    .pipe(gulp.dest("dist/js"));
 });
 
 /**
  * Copy the downloaded files.
  */
-gulp.task('download', function() {
-  return gulp.src('downloads/**/*')
-    .pipe(gulp.dest('dist/downloads'));
+gulp.task("download", function() {
+  return gulp.src("downloads/**/*")
+    .pipe(gulp.dest("dist/downloads"));
 });
 
 /**
  * Copy the html files to the dist folder.
  */
-gulp.task('html', function() {
-  return gulp.src(['*.html', 'CNAME'])
-    .pipe(gulp.dest('dist/'));
+gulp.task("html", function() {
+  return gulp.src(["*.html", "CNAME"])
+    .pipe(gulp.dest("dist/"));
 });
 
 /**
  * Copy images to dist/images folder.
  */
-gulp.task('img', function() {
-  return gulp.src('images/**/*')
-    .pipe(gulp.dest('dist/images'));
+gulp.task("img", function() {
+  return gulp.src("images/**/*")
+    .pipe(gulp.dest("dist/images"));
 });
 
 /**
  * Builds all assets.
  */
-gulp.task('build', ['download', 'sass', 'js', 'html', 'img']);
+gulp.task("build", ["download", "sass", "js", "html", "img"]);
 
 /**
  * Clean dist folder.
  */
-gulp.task('clean', function () {
-  return gulp.src('dist/*', { read: false })
+gulp.task("clean", function () {
+  return gulp.src("dist/*", { read: false })
     .pipe(clean({ force: true }));
 });
 
 /**
  * Clean and build the website.
  */
-gulp.task('prepare-deploy', taskSeq('clean', 'build'));
+gulp.task("prepare-deploy", function(done){
+  deploy = true;
+  runSequence("clean", "build", done);
+});
 
 /**
  * Deploys the website.
  */
-gulp.task('deploy', ['prepare-deploy'], function(){
-  return gulp.src('./dist/**/*')
-    .pipe(ghPages({branch: 'master', message: 'Deployed on [timestamp]'}));
+gulp.task("deploy", ["prepare-deploy"], function(){
+  return gulp.src("./dist/**/*")
+    .pipe(ghPages({branch: "master", message: "Deployed on [timestamp]"}));
 });
 
 /**
  * Starts a web server within dist folder.
  */
-gulp.task('connect', ['build'], function() {
+gulp.task("connect", ["build"], function() {
   connect.server({
-    root: 'dist',
+    root: "dist",
     livereload: true,
     middleware: function(connect, opt) {
       return [
         function(req, res, next) {
           if (!req.url.match(/^\/(|.*\..*)$/)) {
-            req.url = req.url + '.html';
+            req.url = req.url + ".html";
           }
-          var path = 'dist' + req.url
-          if(!require('fs').existsSync(path)){
-            req.url = '/404.html';
+          var path = "dist" + req.url
+          if(!require("fs").existsSync(path)){
+            req.url = "/404.html";
           }
           next();
         }
@@ -134,20 +140,20 @@ gulp.task('connect', ['build'], function() {
 /**
  * Live reload web pages.
  */
-gulp.task('reload', ['html'], function () {
-  return gulp.src('*.html')
+gulp.task("reload", ["html"], function () {
+  return gulp.src("*.html")
     .pipe(connect.reload());
 });
 
 /**
  * Watch files and recompile assets when any file is updated.
  */
-gulp.task('watch', ['build'], function() {
-  gulp.watch(['download/**/*'], watchSeq('download', 'reload'));
-  gulp.watch(['*.html'], ['reload']);
-  gulp.watch(['scss/**/*.scss'], watchSeq('sass', 'reload'));
-  gulp.watch(['js/**/*.js'], watchSeq('js','reload'));
-  gulp.watch(['images/**/*'], watchSeq('img','reload'));
+gulp.task("watch", ["build"], function() {
+  gulp.watch(["download/**/*"], watchSeq("download", "reload"));
+  gulp.watch(["*.html"], ["reload"]);
+  gulp.watch(["scss/**/*.scss"], watchSeq("sass", "reload"));
+  gulp.watch(["js/**/*.js"], watchSeq("js","reload"));
+  gulp.watch(["images/**/*"], watchSeq("img","reload"));
 });
 
-gulp.task('default', ['build', 'connect', 'watch']);
+gulp.task("default", ["build", "connect", "watch"]);
--- js/mixpanel.js
+++ js/mixpanel.js
@@ -1,11 +1,116 @@
 (function(e,a){if(!a.__SV){var b=window;try{var c,l,i,j=b.location,g=j.hash;c=function(a,b){return(l=a.match(RegExp(b+"=([^&]*)")))?l[1]:null};g&&c(g,"state")&&(i=JSON.parse(decodeURIComponent(c(g,"state"))),"mpeditor"===i.action&&(b.sessionStorage.setItem("_mpcehash",g),history.replaceState(i.desiredHash||"",e.title,j.pathname+j.search)))}catch(m){}var k,h;window.mixpanel=a;a._i=[];a.init=function(b,c,f){function e(b,a){var c=a.split(".");2==c.length&&(b=b[c[0]],a=c[1]);b[a]=function(){b.push([a].concat(Array.prototype.slice.call(arguments,
 0)))}}var d=a;"undefined"!==typeof f?d=a[f]=[]:f="mixpanel";d.people=d.people||[];d.toString=function(b){var a="mixpanel";"mixpanel"!==f&&(a+="."+f);b||(a+=" (stub)");return a};d.people.toString=function(){return d.toString(1)+".people (stub)"};k="disable time_event track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config reset people.set people.set_once people.increment people.append people.union people.track_charge people.clear_charges people.delete_user".split(" ");
 for(h=0;h<k.length;h++)e(d,k[h]);a._i.push([b,c,f])};a.__SV=1.2;b=e.createElement("script");b.type="text/javascript";b.async=!0;b.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?MIXPANEL_CUSTOM_LIB_URL:"file:"===e.location.protocol&&"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//)?"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js":"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";c=e.getElementsByTagName("script")[0];c.parentNode.insertBefore(b,c)}})(document,window.mixpanel||[]);
+
 (function(){
-	var productionHost = 'alessiolaiso.com';
-	if (window.location.hostname.toLowerCase().indexOf(productionHost) === -1) {
-	  mixpanel.init("");
-	} else {
-	  mixpanel.init("cf84101d09727884f4b3cf33dd6522ac");
-	}
+  var productionHost = "alessiolaiso.com";
+  if (window.location.hostname.toLowerCase().indexOf(productionHost) === -1) {
+    mixpanel.init("881a6c5ec50692b9f2d964675cc54893");
+  } else {
+    mixpanel.init("cf84101d09727884f4b3cf33dd6522ac");
+  }
 })();
+
+$(function(){
+  var trackCb = function(event){
+    return function(){
+      mixpanel.track(event);
+    }
+  };
+  $(".linkedin-icon").click(trackCb("link:click:linkedin"));
+  $(".twitter-icon").click(trackCb("link:click:twitter"));
+});
+
+var ProjectTracker = function($, opts){
+  var project = opts.project;
+  delete opts.project;
+  var started = false;
+  var done = false;
+  var sectionTiming = 0;
+  var startTime = 0;
+  var currentSection = null;
+  opts.elements = [".project-main"].concat(opts.elements || []).concat([".project-nav"]);
+  var elements = opts.elements;
+  var track = $(window).scrollTop() == 0;
+
+  var hasRead = function(){
+    return localStorage.getItem("project:" + project) === "done";
+  };
+
+  var trackEvent = function(event, props){
+    if(!hasRead()){
+      props["project"] = project;
+      mixpanel.track("project:reading:" + event, props);
+    };
+  };
+
+
+  var time = function(){
+    return new Date().getTime();
+  };
+
+  var elapsedTime = function(){
+    return (time() - startTime) / 1000;
+  };
+
+  $(window).on("beforeunload", function(){
+    if(started && !done){
+      trackEvent("abort", { percent: percent, duration: elapsedTime() });
+    }
+  });
+
+  mixpanel.track("project:page_loaded", { project: project, read: hasRead() });
+  if(!track){
+    mixpanel.track("project:not_tracked", { project: project });
+  }
+
+  return ScrollTracker($, opts, function(data){
+    if(!track ||
+       data.event !== "ScrollTiming" ||
+       data.eventAction !== "Elements" ||
+       localStorage.getItem("project:" + project) === "done"){
+      return;
+    }
+    var label = data.eventLabel;
+
+    if(label === ".project-main"){
+      started = true;
+      startTime = time();
+      return trackEvent("started", { project: project });
+    }
+    if(currentSection != null){
+      var duration = (data.eventTiming - sectionTiming) / 1000;
+      trackEvent("section_read", { project: project, name: currentSection, duration: duration });
+    }
+    if(label === ".project-nav"){
+      done = true;
+      trackEvent("finished", { project: project, duration: elapsedTime() });
+      return localStorage.setItem("project:" + project, "done");
+    }
+    if(opts.elements.indexOf(label) > -1){
+      sectionTiming = data.eventTiming;
+      currentSection = label.substring(1);
+    }
+  });
+}
+
+var ScrollTracker = function($, opts, cb) {
+  if(typeof opts === "function"){
+    cb = opts;
+    opts = {};
+  }
+
+  var track = function() {
+    $(function(){
+      $.scrollDepth({
+        elements: opts.elements || [],
+        pixelDepth: false,
+        eventHandler: function(data){
+          cb(data);
+        }
+      });
+    });
+  };
+
+  return { track: track };
+};
