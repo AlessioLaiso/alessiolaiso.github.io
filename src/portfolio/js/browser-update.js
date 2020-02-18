26bc9e77b83b7ab18a09093591031b3dc76e61d2
AlessioLaiso <alessio.laiso@gmail.com>
2020-02-18 11:41:01 +0000

    Creates Portfolio section

---
+++ src/portfolio/js/browser-update.js
@@ -0,0 +1,12 @@
+var $buoop = {vs:{i:11,f:30,o:15,s:6,c:30},unsupported:false,mobile:false,api:4};
+function $buo_f(){
+ var e = document.createElement("script");
+ e.async=1;
+ e.src = "//browser-update.org/update.min.js";
+ document.body.appendChild(e);
+};
+try {
+	document.addEventListener("DOMContentLoaded", $buo_f,false)}
+catch(e){
+	window.attachEvent("onload", $buo_f)
+}
